bdafae57db0996acc9cd825096810d7b3902df2f
dmitriy2893 <dmitriy2893@gmail.com>
2021-09-19 18:37:29 +0300

    Добавил секции с Телой

---
+++ src/components/Section.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
+	return (
+		<Wrap bgImage={backgroundImg}>
+			<ItemText>
+				<h1>{title}</h1>
+				<p>{description}</p>
+			</ItemText>
+
+			<Buttons>
+				<ButtonGroup>
+					<LeftButton>{leftBtnText}</LeftButton>
+					{rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+				</ButtonGroup>
+				<DownArrow src='/down-arrow.svg' />
+			</Buttons>
+		</Wrap>
+	)
+}
+
+export default Section
+
+const Wrap = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	background-image: url('/model-s.jpg');
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+	background-image: ${(props) => `url("/${props.bgImage}")`};
+`
+
+const ItemText = styled.div`
+	padding-top: 15vh;
+	text-align: center;
+`
+const Buttons = styled.div``
+const ButtonGroup = styled.div`
+	display: flex;
+	margin-bottom: 20px;
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
+`
+const LeftButton = styled.div`
+	background-color: rgba(23, 26, 32, 0.8);
+	height: 40px;
+	width: 256px;
+	color: #fff;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 100px;
+	opacity: 0.85;
+	text-transform: uppercase;
+	font-size: 12px;
+	cursor: pointer;
+	margin: 8px;
+`
+const RightButton = styled(LeftButton)`
+	background: #fff;
+	opacity: 0.65;
+	color: black;
+`
+const DownArrow = styled.img`
+	height: 40px;
+	overflow-x: hidden;
+	animation: animateDown infinite 1.5s;
+`
